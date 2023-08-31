78e25a5fde3a30d173715fd43f5d00a09d30dc33
yoshoku <yoshoku@outlook.com>
2023-08-31 23:21:23 +0900

    refactor: remove unnecessary wrapper class

--- ext/gpt_neox_client/gpt_neox_client.cpp
+++ ext/gpt_neox_client/gpt_neox_client.cpp
@@ -5,33 +5,26 @@ VALUE rb_cGPTParams;
 VALUE rb_cGPTVocab;
 VALUE rb_cGPTNeoXModel;
 
-class GPTNeoXModelWrapper {
-public:
-  struct gpt_neox_model model;
-  GPTNeoXModelWrapper() {}
-  ~GPTNeoXModelWrapper() {}
-};
-
 class RbGPTNeoXModel {
 public:
   static VALUE gpt_neox_model_alloc(VALUE self) {
-    GPTNeoXModelWrapper* ptr = (GPTNeoXModelWrapper*)ruby_xmalloc(sizeof(GPTNeoXModelWrapper));
-    new (ptr) GPTNeoXModelWrapper();
+    gpt_neox_model* ptr = (gpt_neox_model*)ruby_xmalloc(sizeof(gpt_neox_model));
+    new (ptr) gpt_neox_model();
     return TypedData_Wrap_Struct(self, &gpt_neox_model_type, ptr);
   }
 
   static void gpt_neox_model_free(void* ptr) {
-    ((GPTNeoXModelWrapper*)ptr)->~GPTNeoXModelWrapper();
+    ((gpt_neox_model*)ptr)->~gpt_neox_model();
     ruby_xfree(ptr);
   }
 
   static size_t gpt_neox_model_size(const void* ptr) {
-    return sizeof(*((GPTNeoXModelWrapper*)ptr));
+    return sizeof(*((gpt_neox_model*)ptr));
   }
 
-  static GPTNeoXModelWrapper* get_gpt_neox_model(VALUE self) {
-    GPTNeoXModelWrapper* ptr = nullptr;
-    TypedData_Get_Struct(self, GPTNeoXModelWrapper, &gpt_neox_model_type, ptr);
+  static gpt_neox_model* get_gpt_neox_model(VALUE self) {
+    gpt_neox_model* ptr = nullptr;
+    TypedData_Get_Struct(self, gpt_neox_model, &gpt_neox_model_type, ptr);
     return ptr;
   }
 
@@ -54,33 +47,26 @@ const rb_data_type_t RbGPTNeoXModel::gpt_neox_model_type = {
   RUBY_TYPED_FREE_IMMEDIATELY
 };
 
-class GPTVocabWrapper {
-public:
-  struct gpt_vocab vocab;
-  GPTVocabWrapper() {}
-  ~GPTVocabWrapper() {}
-};
-
 class RbGPTVocab {
 public:
   static VALUE gpt_vocab_alloc(VALUE self) {
-    GPTVocabWrapper* ptr = (GPTVocabWrapper*)ruby_xmalloc(sizeof(GPTVocabWrapper));
-    new (ptr) GPTVocabWrapper();
+    gpt_vocab* ptr = (gpt_vocab*)ruby_xmalloc(sizeof(gpt_vocab));
+    new (ptr) gpt_vocab();
     return TypedData_Wrap_Struct(self, &gpt_vocab_type, ptr);
   }
 
   static void gpt_vocab_free(void* ptr) {
-    ((GPTVocabWrapper*)ptr)->~GPTVocabWrapper();
+    ((gpt_vocab*)ptr)->~gpt_vocab();
     ruby_xfree(ptr);
   }
 
   static size_t gpt_vocab_size(const void* ptr) {
-    return sizeof(*((GPTVocabWrapper*)ptr));
+    return sizeof(*((gpt_vocab*)ptr));
   }
 
-  static GPTVocabWrapper* get_gpt_vocab(VALUE self) {
-    GPTVocabWrapper* ptr = nullptr;
-    TypedData_Get_Struct(self, GPTVocabWrapper, &gpt_vocab_type, ptr);
+  static gpt_vocab* get_gpt_vocab(VALUE self) {
+    gpt_vocab* ptr = nullptr;
+    TypedData_Get_Struct(self, gpt_vocab, &gpt_vocab_type, ptr);
     return ptr;
   }
 
@@ -103,33 +89,26 @@ const rb_data_type_t RbGPTVocab::gpt_vocab_type = {
   RUBY_TYPED_FREE_IMMEDIATELY
 };
 
-class GPTParamsWrapper {
-public:
-  struct gpt_params params;
-  GPTParamsWrapper() {}
-  ~GPTParamsWrapper() {}
-};
-
 class RbGPTParams {
 public:
   static VALUE gpt_params_alloc(VALUE self) {
-    GPTParamsWrapper* ptr = (GPTParamsWrapper*)ruby_xmalloc(sizeof(GPTParamsWrapper));
-    new (ptr) GPTParamsWrapper();
+    gpt_params* ptr = (gpt_params*)ruby_xmalloc(sizeof(gpt_params));
+    new (ptr) gpt_params();
     return TypedData_Wrap_Struct(self, &gpt_params_type, ptr);
   }
 
   static void gpt_params_free(void* ptr) {
-    ((GPTParamsWrapper*)ptr)->~GPTParamsWrapper();
+    ((gpt_params*)ptr)->~gpt_params();
     ruby_xfree(ptr);
   }
 
   static size_t gpt_params_size(const void* ptr) {
-    return sizeof(*((GPTParamsWrapper*)ptr));
+    return sizeof(*((gpt_params*)ptr));
   }
 
-  static GPTParamsWrapper* get_gpt_params(VALUE self) {
-    GPTParamsWrapper* ptr = nullptr;
-    TypedData_Get_Struct(self, GPTParamsWrapper, &gpt_params_type, ptr);
+  static gpt_params* get_gpt_params(VALUE self) {
+    gpt_params* ptr = nullptr;
+    TypedData_Get_Struct(self, gpt_params, &gpt_params_type, ptr);
     return ptr;
   }
 
@@ -165,138 +144,138 @@ private:
 
   // seed
   static VALUE _gpt_params_set_seed(VALUE self, VALUE seed) {
-    GPTParamsWrapper* ptr = get_gpt_params(self);
-    ptr->params.seed = NUM2INT(seed);
-    return INT2NUM(ptr->params.seed);
+    gpt_params* ptr = get_gpt_params(self);
+    ptr->seed = NUM2INT(seed);
+    return INT2NUM(ptr->seed);
   }
 
   static VALUE _gpt_params_get_seed(VALUE self) {
-    GPTParamsWrapper* ptr = get_gpt_params(self);
-    return INT2NUM(ptr->params.seed);
+    gpt_params* ptr = get_gpt_params(self);
+    return INT2NUM(ptr->seed);
   }
 
   // n_threads
   static VALUE _gpt_params_set_n_threads(VALUE self, VALUE n_threads) {
-    GPTParamsWrapper* ptr = get_gpt_params(self);
-    ptr->params.n_threads = NUM2INT(n_threads);
-    return INT2NUM(ptr->params.n_threads);
+    gpt_params* ptr = get_gpt_params(self);
+    ptr->n_threads = NUM2INT(n_threads);
+    return INT2NUM(ptr->n_threads);
   }
 
   static VALUE _gpt_params_get_n_threads(VALUE self) {
-    GPTParamsWrapper* ptr = get_gpt_params(self);
-    return INT2NUM(ptr->params.n_threads);
+    gpt_params* ptr = get_gpt_params(self);
+    return INT2NUM(ptr->n_threads);
   }
 
   // n_predict
   static VALUE _gpt_params_set_n_predict(VALUE self, VALUE n_predict) {
-    GPTParamsWrapper* ptr = get_gpt_params(self);
-    ptr->params.n_predict = NUM2INT(n_predict);
-    return INT2NUM(ptr->params.n_predict);
+    gpt_params* ptr = get_gpt_params(self);
+    ptr->n_predict = NUM2INT(n_predict);
+    return INT2NUM(ptr->n_predict);
   }
 
   static VALUE _gpt_params_get_n_predict(VALUE self) {
-    GPTParamsWrapper* ptr = get_gpt_params(self);
-    return INT2NUM(ptr->params.n_predict);
+    gpt_params* ptr = get_gpt_params(self);
+    return INT2NUM(ptr->n_predict);
   }
 
   // n_batch
   static VALUE _gpt_params_set_n_batch(VALUE self, VALUE n_batch) {
-    GPTParamsWrapper* ptr = get_gpt_params(self);
-    ptr->params.n_batch = NUM2INT(n_batch);
-    return INT2NUM(ptr->params.n_batch);
+    gpt_params* ptr = get_gpt_params(self);
+    ptr->n_batch = NUM2INT(n_batch);
+    return INT2NUM(ptr->n_batch);
   }
 
   static VALUE _gpt_params_get_n_batch(VALUE self) {
-    GPTParamsWrapper* ptr = get_gpt_params(self);
-    return INT2NUM(ptr->params.n_batch);
+    gpt_params* ptr = get_gpt_params(self);
+    return INT2NUM(ptr->n_batch);
   }
 
   // top_k
   static VALUE _gpt_params_set_top_k(VALUE self, VALUE top_k) {
-    GPTParamsWrapper* ptr = get_gpt_params(self);
-    ptr->params.top_k = NUM2INT(top_k);
-    return INT2NUM(ptr->params.top_k);
+    gpt_params* ptr = get_gpt_params(self);
+    ptr->top_k = NUM2INT(top_k);
+    return INT2NUM(ptr->top_k);
   }
 
   static VALUE _gpt_params_get_top_k(VALUE self) {
-    GPTParamsWrapper* ptr = get_gpt_params(self);
-    return INT2NUM(ptr->params.top_k);
+    gpt_params* ptr = get_gpt_params(self);
+    return INT2NUM(ptr->top_k);
   }
 
   // top_p
   static VALUE _gpt_params_set_top_p(VALUE self, VALUE top_p) {
-    GPTParamsWrapper* ptr = get_gpt_params(self);
-    ptr->params.top_p = NUM2DBL(top_p);
-    return DBL2NUM(ptr->params.top_p);
+    gpt_params* ptr = get_gpt_params(self);
+    ptr->top_p = NUM2DBL(top_p);
+    return DBL2NUM(ptr->top_p);
   }
 
   static VALUE _gpt_params_get_top_p(VALUE self) {
-    GPTParamsWrapper* ptr = get_gpt_params(self);
-    return DBL2NUM(ptr->params.top_p);
+    gpt_params* ptr = get_gpt_params(self);
+    return DBL2NUM(ptr->top_p);
   }
 
   // temp
   static VALUE _gpt_params_set_temp(VALUE self, VALUE temp) {
-    GPTParamsWrapper* ptr = get_gpt_params(self);
-    ptr->params.temp = NUM2DBL(temp);
-    return DBL2NUM(ptr->params.temp);
+    gpt_params* ptr = get_gpt_params(self);
+    ptr->temp = NUM2DBL(temp);
+    return DBL2NUM(ptr->temp);
   }
 
   static VALUE _gpt_params_get_temp(VALUE self) {
-    GPTParamsWrapper* ptr = get_gpt_params(self);
-    return DBL2NUM(ptr->params.temp);
+    gpt_params* ptr = get_gpt_params(self);
+    return DBL2NUM(ptr->temp);
   }
 
   // repeat_last_n
   static VALUE _gpt_params_set_repeat_last_n(VALUE self, VALUE repeat_last_n) {
-    GPTParamsWrapper* ptr = get_gpt_params(self);
-    ptr->params.repeat_last_n = NUM2INT(repeat_last_n);
-    return INT2NUM(ptr->params.repeat_last_n);
+    gpt_params* ptr = get_gpt_params(self);
+    ptr->repeat_last_n = NUM2INT(repeat_last_n);
+    return INT2NUM(ptr->repeat_last_n);
   }
 
   static VALUE _gpt_params_get_repeat_last_n(VALUE self) {
-    GPTParamsWrapper* ptr = get_gpt_params(self);
-    return INT2NUM(ptr->params.repeat_last_n);
+    gpt_params* ptr = get_gpt_params(self);
+    return INT2NUM(ptr->repeat_last_n);
   }
 
   // repeat_penalty
   static VALUE _gpt_params_set_repeat_penalty(VALUE self, VALUE repeat_penalty) {
-    GPTParamsWrapper* ptr = get_gpt_params(self);
-    ptr->params.repeat_penalty = NUM2DBL(repeat_penalty);
-    return DBL2NUM(ptr->params.repeat_penalty);
+    gpt_params* ptr = get_gpt_params(self);
+    ptr->repeat_penalty = NUM2DBL(repeat_penalty);
+    return DBL2NUM(ptr->repeat_penalty);
   }
 
   static VALUE _gpt_params_get_repeat_penalty(VALUE self) {
-    GPTParamsWrapper* ptr = get_gpt_params(self);
-    return DBL2NUM(ptr->params.repeat_penalty);
+    gpt_params* ptr = get_gpt_params(self);
+    return DBL2NUM(ptr->repeat_penalty);
   }
 
   // model
   static VALUE _gpt_params_set_model(VALUE self, VALUE model) {
-    GPTParamsWrapper* ptr = get_gpt_params(self);
+    gpt_params* ptr = get_gpt_params(self);
     std::string model_str(StringValueCStr(model));
-    ptr->params.model = model_str;
+    ptr->model = model_str;
     RB_GC_GUARD(model);
-    return rb_str_new_cstr(ptr->params.model.c_str());
+    return rb_str_new_cstr(ptr->model.c_str());
   }
 
   static VALUE _gpt_params_get_model(VALUE self) {
-    GPTParamsWrapper* ptr = get_gpt_params(self);
-    return rb_str_new_cstr(ptr->params.model.c_str());
+    gpt_params* ptr = get_gpt_params(self);
+    return rb_str_new_cstr(ptr->model.c_str());
   }
 
   // prompt
   static VALUE _gpt_params_set_prompt(VALUE self, VALUE prompt) {
-    GPTParamsWrapper* ptr = get_gpt_params(self);
+    gpt_params* ptr = get_gpt_params(self);
     std::string prompt_str(StringValueCStr(prompt));
-    ptr->params.prompt = prompt_str;
+    ptr->prompt = prompt_str;
     RB_GC_GUARD(prompt);
-    return rb_str_new_cstr(ptr->params.prompt.c_str());
+    return rb_str_new_cstr(ptr->prompt.c_str());
   }
 
   static VALUE _gpt_params_get_prompt(VALUE self) {
-    GPTParamsWrapper* ptr = get_gpt_params(self);
-    return rb_str_new_cstr(ptr->params.prompt.c_str());
+    gpt_params* ptr = get_gpt_params(self);
+    return rb_str_new_cstr(ptr->prompt.c_str());
   }
 };
 
