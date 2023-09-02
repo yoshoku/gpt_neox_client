8cf7db954ccc18035bd7dd329986835f5f50df99
yoshoku <yoshoku@outlook.com>
2023-09-02 22:26:03 +0900

    chore: add build option for macOS

--- ext/gpt_neox_client/extconf.rb
+++ ext/gpt_neox_client/extconf.rb
@@ -22,4 +22,28 @@ $INCFLAGS << ' -I$(srcdir)/src/ggml'
 $VPATH << '$(srcdir)/src'
 $VPATH << '$(srcdir)/src/ggml'
 
+if RUBY_PLATFORM.match?(/darwin|linux|bsd/) && try_compile('#include <stdio.h>', '-pthread')
+  $CFLAGS << ' -pthread'
+  $CXXFLAGS << ' -pthread'
+end
+
+if RUBY_PLATFORM.match?(/darwin/)
+  if have_framework('Accelerate')
+    $CFLAGS << ' -DGGML_USE_ACCELERATE'
+  else
+    warning 'Accelerate framework is not found.'
+  end
+end
+
 create_makefile('gpt_neox_client/gpt_neox_client')
+
+if RUBY_PLATFORM.match?(/darwin/)
+  File.open('Makefile', 'a') do |f|
+    f.puts "\nggml-metal.o: ggml-metal.m ggml-metal.h"
+    f.puts "\t$(CC) $(CFLAGS) -c $< -o $@"
+  end
+
+  metal_path = File.expand_path("#{__dir__}/src/ggml/ggml-metal.metal")
+  dest_path = File.expand_path("#{__dir__}/../../lib/gpt_neox_client/")
+  FileUtils.cp(metal_path, dest_path)
+end
